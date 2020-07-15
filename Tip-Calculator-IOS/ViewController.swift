d4de37243465caf717361a24e79f7006513ec053
Saad Mahboob <smahboob@fandm.edu>
2020-07-15 22:58:09 +0500

    Initial Commit

---
+++ Tip-Calculator-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip-Calculator-IOS
+//
+//  Created by Saad Mahboob on 7/15/20.
+//  Copyright © 2020 Saad Mahboob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
