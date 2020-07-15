5f4ddf82c02e1df2412975b5b071ea7fca0cfd31
Saad Mahboob <smahboob@fandm.edu>
2020-07-15 23:10:32 +0500

    update files

--- Tip-Calculator-IOS/ViewController.swift
+++ Tip-Calculator-IOS/ViewController.swift
@@ -1,20 +1,74 @@
-//
 //  ViewController.swift
-//  Tip-Calculator-IOS
+//  Tip-Calculator
 //
 //  Created by Saad Mahboob on 7/15/20.
 //  Copyright © 2020 Saad Mahboob. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate {
 
+    @IBOutlet weak var textFiled: UITextField!
+    @IBOutlet weak var slider: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var plusSign: UILabel!
+    @IBOutlet weak var equalSign: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var slider2: UISegmentedControl!
+    @IBOutlet weak var dollarSign: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        plusSign.isHidden = true
+        equalSign.isHidden = true
+        slider2.isHidden = true
+        slider.isHidden = true
+        dollarSign.isHidden = true
+        textFiled.delegate = self
     }
+    
+    @IBAction func pressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        plusSign.isHidden = false
+        equalSign.isHidden = false
+        slider2.isHidden = false
+        slider.isHidden = false
+        dollarSign.isHidden = false
 
-
+        let billAmount = Double(textFiled.text!) ?? 0
+        let percentages = [0.1,0.2,0.3]
+        
+        let tipIndex = slider.selectedSegmentIndex
+        
+        let tip = billAmount * percentages[tipIndex]
+        let total = billAmount + tip
+        
+        tipLabel.text   = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        let indexOfPeople = slider2.selectedSegmentIndex
+        
+        if(indexOfPeople == 0){
+            let newBill = total / 2
+            detailLabel.text = String(format: "$%.2f each", newBill)
+        }
+        else if (indexOfPeople == 1){
+            let newBill = total / 3
+            detailLabel.text = String(format: "$%.2f each", newBill)
+        }
+        
+        else{
+            let newBill = total / 4
+            detailLabel.text = String(format: "$%.2f each", newBill)
+        }
+        
+    }
 }
+
+
+
 
